28ac7ee4c48930d6ccdfffb42534332d0353816b
panoke <3214736+panoke@users.noreply.github.com>
2026-09-14 11:58:47 +0100

    add truncateNumber function to shorten result for display

---
+++ script.js
@@ -0,0 +1,42 @@
+function addNumbers(a, b) {
+    return a + b
+}
+
+function subtractNumbers(a, b) {
+    return a - b
+}
+
+function multiplyNumbers(a, b) {
+    return a * b
+}
+
+function divideNumbers(a, b) {
+    return a / b
+}
+
+// rounds number to given length of digits
+function truncateNumber (number, digits = 10) {
+
+    // check that digits value is positive integer
+    if(!Number.isInteger(digits) || digits <= 0) {
+        throw new Error("Digits must be a positive integer.");
+    }
+
+    // if not a number return and stop processing
+    if (Number.isNaN(number)) { return NaN };
+
+    // if number is not finite return as is as cannot process
+    if (!Number.isFinite(number)) { return number }
+
+    // convert number to positive to not include sign in length calculations
+    const sign = number > 0 ? 1 : -1;
+    number = sign * number;    
+
+    // truncate number to number of digits and remove trailing zeros
+    number = parseFloat(number.toPrecision(digits))
+
+    // if number is greater than number of digits return in exponential notation
+    const numberDigits = number.toString().replace(".", "").length
+    if (numberDigits > digits) { return sign * number.toExponential(digits - 1) }
+    else { return sign * number }
+}
